61e04a282b2abad803b0c1dd5a5c6350fce7477b
Chad Armitstead <TheHob@users.noreply.github.com>
2018-11-26 11:57:40 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -87,6 +87,7 @@ module "hashistack-us-east" {
   cluster_name             = "${random_id.environment_name.hex}-us-east-1"
   environment_name         = "${random_id.environment_name.hex}"
   remote_regions           = ["us-west-2"]
+  instance_type            = "t2.small"
   instance_profile         = "${module.hashistack-instance-profile.policy}"
   ssh_key_name             = "${random_id.environment_name.hex}-us-east"
   public_key_data          = "${module.ssh.public_key_data}"
